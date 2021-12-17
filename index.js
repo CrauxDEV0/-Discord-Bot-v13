9c67ea4dba74c7f730cf6c41341e1de707377cb8
CrauxDEV0 <96301244+CrauxDEV0@users.noreply.github.com>
2021-12-17 20:11:37 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const {Collection, Client, Discord} = require('discord.js')
+const fs = require('fs');
+const mongo = require("./mongo.js")
+const client = new Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+        "GUILD_PRESENCES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING",
+    ],
+});
+
+module.exports = client;
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is online')
+});
+
+app.listen(3000, () => {
+  console.log('Server Listening 3000');
+});
+
+
+
+      
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+
+["command", "event"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+
+
+client.login(process.env.TOKEN)
